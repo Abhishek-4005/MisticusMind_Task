dfbe9a47e53a6fd36298829cc500bd729d592a03
AbhishekLohar-ANT <abhishek.lohar@antllp.com>
2024-09-19 18:16:38 +0530

    Update environments.ts

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,7 +1,8 @@
 
 export const environment = {
     production: false,
-    apiUrl: "http://100.28.10.86/api/"
-    // 'http://98.80.132.125:8000/api/'
+    apiUrl: 
+    // "http://100.28.10.86/api/"
+    'http://98.80.132.125:8000/api/'
   };
   
